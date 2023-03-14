694c890ce2c2a0a60ab0b41281507e8344746656
Taylor Remund <remundtaylor3@gmail.com>
2023-03-14 11:52:05 -0600

    init commit

---
+++ server.py
@@ -0,0 +1,16 @@
+"""Server for movie ratings app."""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Replace this with routes and view functions!
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", debug=True)
