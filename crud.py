90071a17abd76c5266c1d14e1dc4d9bcdb0c7821
Taylor Remund <remundtaylor3@gmail.com>
2023-03-15 19:26:07 -0600

    almost finished, rating system not yet working

--- crud.py
+++ crud.py
@@ -13,8 +13,10 @@ def get_users():
 def get_user_by_id(user_id):
     return User.query.get(user_id)
 
+def get_user_by_email(email):
+    return User.query.filter(User.email == email).first()
+
 def create_movie(title, overview, release_date, poster_path):
-    """Create and return a new movie."""
 
     movie = Movie(
         title=title,
@@ -33,14 +35,17 @@ def get_movie_by_id(movie_id):
     return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
-    """Create and return a new rating."""
 
     rating = Rating(user=user, movie=movie, score=score)
 
     return rating
 
+def update_rating(rating_id, new_score):
+    rating = Rating.query.get(rating_id)
+    rating.score = new_score
 
 if __name__ == '__main__':
+
     from server import app
 
     connect_to_db(app)
